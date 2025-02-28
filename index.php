a8519d4bd54fa252675c4ec4633db78800f096db
nunu1702 <antibiotik039@gmail.com>
2025-02-28 21:15:51 +0700

    menambahlist

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
     
 <h1>coba git remote</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, ratione impedit corporis esse quibusdam quod eveniet eaque qui similique sed ea beatae? Veniam porro, autem natus aperiam quia cum sapiente!</p>
+<ul>
+    <li> stu</li>
+    <li> dua</li>
+</ul>
 </body>
 </html>
